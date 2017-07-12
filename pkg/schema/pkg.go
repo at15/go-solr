3093851aa4c0ef549fb62fd286eb7fa8fe48167c
at15 <at15@outlook.com>
2017-07-11 20:57:17 -0700

    Init schema and util package

---
+++ pkg/schema/pkg.go
@@ -0,0 +1,7 @@
+package schema
+
+import (
+	"github.com/at15/go-solr/pkg/util"
+)
+
+var log = util.Logger.NewEntryWithPkg("go-solr.schema")
---
+++ pkg/util/log.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	dlog "github.com/dyweb/gommon/log"
+)
+
+// Logger is the default logger with info level
+var Logger = dlog.NewLogger()
+
+// Short name use in util package
+var log = Logger.NewEntryWithPkg("go-solr.util")
+
+func init() {
+	f := dlog.NewTextFormatter()
+	f.EnableColor = true
+	Logger.Formatter = f
+	Logger.Level = dlog.InfoLevel
+}
